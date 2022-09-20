77bb129897b1e28e3cb69536b2911767eff2cbe6
Mehmet Baran <59487546+baranmmm@users.noreply.github.com>
2022-09-20 16:58:56 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,5 +3,6 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("practice makes better");
+        //Write some code here...
     }
 }
